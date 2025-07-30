8d16c786de0418777cb524e5f7743df0fa4f3dfd
sifulsidd <sifulsiddiki@yahoo.com>
2025-07-29 23:27:58 -0400

    Main program up and running

--- main.go
+++ main.go
@@ -11,12 +11,16 @@ import (
 )
 
 func main(){
+	// gets the usernmae first
 	user, err := user.Current()
 
+	// similar to an except block 
 	if err != nil {
+		// panic is same as raise for errors
 		panic(err)
 	}
 
+	// gets user and allows program to run
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 
 	fmt.Printf("Feel free to type in commands\n")
